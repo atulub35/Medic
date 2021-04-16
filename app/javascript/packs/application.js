56395339cedcdecc51782570b8313b031246e0f7
Atul Ubarhande <atul.ubarhande@promobitech.com>
2021-04-16 09:38:08 +0530

    added working ticket and comments model

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,13 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import "@hotwired/turbo-rails"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+import "../controllers"
 Rails.start()
+// Turbolinks.start()
 ActiveStorage.start()
 
-import "controllers"
+require("trix")
+require("@rails/actiontext")
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tickets do
+    resources :comments, only: [:new, :create]
+  end
+  root to: 'tickets#index'
 end
