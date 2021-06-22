dce8c2f6727639377a090eb50db3f30c6ed77a1c
Atul Ubarhande <atul.ubarhande@promobitech.com>
2021-06-22 15:44:39 +0530

    disable redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   get '/auth/logout' => 'auth0#logout'
-  get '/auth/redirect' => 'auth0#redirect'
+  # get '/auth/redirect' => 'auth0#redirect'
   get '/' => redirect('/procedures') #'#procedures'
       resources :procedures do
         resources :comments
